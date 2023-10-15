d8843210aff1cd08b2fb6a2905efcdc60da54323
NurmuhammadNikita <888legolas777@gmail.com>
2023-10-15 17:58:54 +0500

    Добавьте файлы проекта.

---
+++ Vazifa19.09/FileSearcher.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vazifa19._09
+{
+    public class FileSearcher
+    {
+
+
+        public void GetPathOfFile(string getfolder)
+        {
+            //Console.Write("File nomini kiriting: ");
+
+            DriveInfo[] drives = DriveInfo.GetDrives();
+
+            foreach (DriveInfo drive in drives)
+            {
+                if (SearchInDir(drive.Name, getfolder))
+                {
+                    return;
+                }
+
+            }
+            /*if (file.Split('.').Length==2)
+            {
+                /*if (SearchInDir("D:\\", getfolder))
+                {
+                    return;
+                }
+                DriveInfo[] drives = DriveInfo.GetDrives();
+
+                foreach(DriveInfo drive in drives)
+                {
+                    if (SearchInDir(drive.Name, file))
+                    {
+                        return;
+                    }
+
+                }
+            }*/
+
+        }
+        private bool SearchInDir(string path, string file)
+        {
+            string[] files;
+            try
+            {
+                //Console.WriteLine(path);
+                files = Directory.GetFiles(path);
+            }
+            catch
+            {
+                return false;
+            }
+
+            string filepath = Path.Combine(path, file);
+            if (files.Contains(filepath))
+            {
+                Console.WriteLine(filepath);
+                return true;
+            }
+            string[] dirs = Directory.GetDirectories(path);
+            foreach (var dir in dirs)
+            {
+                string path1 = Path.Combine(path, dir);
+                bool check = SearchInDir(path1, file);
+                if (check)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Vazifa19.09/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Vazifa19._09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FileSearcher fileSearcher = new FileSearcher();
+            fileSearcher.GetPathOfFile("");
+
+            //data yozib olish uchun
+            using (FileStream file = new FileStream(@"fizika\baza.txt", FileMode.Open))
+            {
+                using (StreamWriter writer = new StreamWriter(file))
+                {
+                    for (int i = 0; i < 100; i++)
+                    {
+                        writer.WriteLine(i);
+                    }
+                }
+            }
+        }
+    }
+}
